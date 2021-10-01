e6eaf95bd5dd500bf3f8ae0e1dff6e7f1c62d772
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-30 23:43:35 -0400

    logic added to exercise31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -4,31 +4,56 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution31 {
+    private int userAge;
+    private int restingHR;
+    private static final Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
+        Solution31 solution = new Solution31();
         //get age and restingHR from the user
+        solution.userAge = solution.getUserAge();
+        solution.restingHR = solution.getRestingHeartRate();
         //prints age and resting heart rate
+        System.out.printf("Resting Pulse: %d\t\tAge: %d%n", solution.restingHR, solution.userAge);
         //calls the print table function
+        solution.printTable();
     }
 
     public int getUserAge() {
         //prompts the user for their age
+        System.out.println("PLease enter your age.");
         //returns the user's age
+        return input.nextInt();
     }
 
     public int getRestingHeartRate() {
         //prompts the user for their resting heart rate
+        System.out.println("PLease enter your resting heart rate.");
         //returns the user's resting heart rate
+        return input.nextInt();
     }
 
-    public int karvonenHeartRateCalc(int age, int restingHR) {
+    public int karvonenHeartRateCalc(int age, int restingHR, double intensity) {
         //returns the heart rate using the formula
+        return (int) Math.round((((220-age)-restingHR)*intensity)+restingHR);
     }
 
     public void printTable() {
+
+        char column = '|';
+        System.out.printf("Intensity\t%cRate%n", column);
+        System.out.printf("------------|--------%n");
         //loops starting at 55 and increments by 5
+        for(int i = 55;i <= 95;i+=5) {
             //prints out the intensity followed by a dashed line
+            System.out.printf("%d%%\t\t\t%c", i, column);
             //prints out the karvonen heart rate
+            System.out.printf("%d bpm", karvonenHeartRateCalc(userAge, restingHR, i/100.0));
             //prints out %n
+            System.out.printf("%n");
+        }
     }
 }
--- exercise31/src/test/java/baseline/Solution31Test.java
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -8,6 +8,6 @@ class Solution31Test {
     @Test
     void karvonenHeartRateCalcTest() {
         Solution31 solution = new Solution31();
-        assertEquals(138, solution.karvonenHeartRateCalc(22, 65));
+        assertEquals(138, solution.karvonenHeartRateCalc(22, 65, 0.55));
     }
 }
