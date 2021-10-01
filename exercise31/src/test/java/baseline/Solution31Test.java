42d1d349c2e9568fe3ff7b290dfaed0f22da4df5
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-30 23:46:16 -0400

    added more unit tests/exercise31 finished

--- exercise31/src/test/java/baseline/Solution31Test.java
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -6,8 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Solution31Test {
     @Test
-    void karvonenHeartRateCalcTest() {
+    void karvonenHeartRateCalcTest1() {
         Solution31 solution = new Solution31();
         assertEquals(138, solution.karvonenHeartRateCalc(22, 65, 0.55));
+    }
+
+    @Test
+    void karvonenHeartRateCalcTest2() {
+        Solution31 solution = new Solution31();
+        assertEquals(142, solution.karvonenHeartRateCalc(50, 90, 0.65));
+    }
+
+    @Test
+    void karvonenHeartRateCalcTest3() {
+        Solution31 solution = new Solution31();
+        assertEquals(209, solution.karvonenHeartRateCalc(4, 80, 0.95));
     }
 }
