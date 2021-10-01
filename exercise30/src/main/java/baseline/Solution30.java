e53052890bd24c016fd3a73f341983532c5e9524
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-30 22:52:42 -0400

    logic added/exercise30 finished

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -6,15 +6,24 @@ package baseline;
 
 public class Solution30 {
     public static void main(String[] args) {
+        Solution30 solution = new Solution30();
         //calls the print table function
+        solution.printMultiplicationTable();
+
     }
 
     public void printMultiplicationTable() {
         //uses two nested for loops each iterating from 1 to 12 inclusive
         //calls the multiplicationFunction with the loop variables as inputs
-        //prints out the result each time
-        //use %-10.3 to create the columns
-        //uses %n once the inner loop completes once
+        for(int i = 1;i <= 12;i++) {
+            for(int j = 1;j <= 12;j++) {
+                //prints out the result each time
+                //use %4 to create the columns
+                System.out.printf("%4d", multiplicationFunction(i, j));
+            }
+            //uses %n once the inner loop completes once
+            System.out.printf("%n");
+        }
     }
 
     public int multiplicationFunction(int a, int b) {
