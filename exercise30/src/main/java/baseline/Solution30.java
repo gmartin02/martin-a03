e4f56ae3d5cf899a21fe75c56a9b09ffb8aa38b7
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-30 22:47:19 -0400

    exercise30 skeleton

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Martin
+ */
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+        //calls the print table function
+    }
+
+    public void printMultiplicationTable() {
+        //uses two nested for loops each iterating from 1 to 12 inclusive
+        //calls the multiplicationFunction with the loop variables as inputs
+        //prints out the result each time
+        //use %-10.3 to create the columns
+        //uses %n once the inner loop completes once
+    }
+
+    public int multiplicationFunction(int a, int b) {
+        //returns a * b to be printed
+        return a*b;
+    }
+}
---
+++ exercise30/src/test/java/baseline/Solution30Test.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution30Test {
+    @Test
+    void multiplicationFunctionTest() {
+        Solution30 solution = new Solution30();
+        assertEquals(12, solution.multiplicationFunction(4, 3));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+    void multiplicationFunctionTest(int a) {
+        Solution30 solution = new Solution30();
+        assertEquals(a*5, solution.multiplicationFunction(a, 5));
+    }
+
+}
