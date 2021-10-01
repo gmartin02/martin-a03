9de85a7ac00683578032b6a94cfedcbf27485f77
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-30 22:19:06 -0400

    added logic and exception handling

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -4,25 +4,41 @@
  */
 package baseline;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Solution29 {
-    private boolean done = false;
     private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //calls user input function
-        //calls rule of 72 function
+        Solution29 solution = new Solution29();
+
+        solution.ruleOf72Calculator(solution.rateOfReturnInput());
     }
 
     public double rateOfReturnInput() {
         //while !done ask for the rate of return
-        //if a valid rate is entered the done flag is set to true to exit the loop
-        //throws exception if there is either 0 entered or a number is not entered
-        //when the exception is caught it tells the user
+        while(true) {
+            System.out.println("Please enter the rate of return: ");
+
+            try {
+                if(input.nextDouble() <= 0) {
+                    //throws exception if there is either 0 entered or a number is not entered
+                    throw new IllegalArgumentException();
+                }
+                break;
+            } catch (IllegalArgumentException | InputMismatchException e){
+                //when the exception is caught it tells the user
+                System.out.println("Sorry, that is not a valid input.");
+                input.nextLine();
+            }
+
+        }
         //returns a valid user input
+        return input.nextDouble();
     }
     public double ruleOf72Calculator(double rateOfReturn) {
         //returns 72 divided by the passed rateOfReturn
+        return 72/rateOfReturn;
     }
 }
--- exercise29/src/test/java/baseline/Solution29Test.java
+++ exercise29/src/test/java/baseline/Solution29Test.java
@@ -1,16 +1,11 @@
 package baseline;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution29Test {
-    private static final Scanner input = new Scanner(System.in);
 
     @ParameterizedTest
     @ValueSource(doubles = {1, 2, 3, 4})
