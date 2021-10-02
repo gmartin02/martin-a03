8ebe5cbed5cacde0e64f35d9406ce16c14defa57
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-02 16:49:58 -0400

    exercise33 logic added and test cases passed

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -1,23 +1,35 @@
 package baseline;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution33 {
+    private static final Scanner input = new Scanner(System.in);
     public final String[] answers = {"Yes", "No", "Maybe", "Ask again later."};
-    //
+    private static final Random random = new Random();
+
     public static void main(String[] args) {
+        Solution33 solution = new Solution33();
         //prompt the user for a question
+        solution.askUserAQuestion();
         //get a random number from the RNG method
         //print out the answer using the number generated as the index
+        System.out.printf("%s", solution.answerQuestion(solution.randomNumberGenerator()));
     }
 
     public void askUserAQuestion() {
-        //ask the user a question
+        //ask the user for a question
+        System.out.print("What's your question?\n>");
+        input.next();
     }
 
     public int randomNumberGenerator() {
         //returns a random number between 1 and 4
+        return random.nextInt(4);
     }
 
     public String answerQuestion(int rng) {
         //returns answers[rng] as the String
+        return answers[rng];
     }
 }
