625b98b25a1c3d4dbf6bae12ef8b0277a0809d00
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-02 16:38:59 -0400

    exercise33 skeleton

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,23 @@
+package baseline;
+
+public class Solution33 {
+    public final String[] answers = {"Yes", "No", "Maybe", "Ask again later."};
+    //
+    public static void main(String[] args) {
+        //prompt the user for a question
+        //get a random number from the RNG method
+        //print out the answer using the number generated as the index
+    }
+
+    public void askUserAQuestion() {
+        //ask the user a question
+    }
+
+    public int randomNumberGenerator() {
+        //returns a random number between 1 and 4
+    }
+
+    public String answerQuestion(int rng) {
+        //returns answers[rng] as the String
+    }
+}
---
+++ exercise33/src/test/java/baseline/Solution33Test.java
@@ -0,0 +1,20 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution33Test {
+    @Test
+    void randomNumberGeneratorTest() {
+        Solution33 solution = new Solution33();
+        assertTrue(solution.randomNumberGenerator() >= 0 && solution.randomNumberGenerator() < 4);
+    }
+
+    @Test
+    void answerQuestionTest() {
+        Solution33 solution = new Solution33();
+        int rng = solution.randomNumberGenerator();
+        assertEquals(solution.answers[rng], solution.answerQuestion(rng));
+    }
+}
