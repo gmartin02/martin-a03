b2cd8fc999c25fdcd853b8f724392eed2adbbb11
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-02 22:06:33 -0400

    exercise38 skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Martin
+ */
+package baseline;
+
+public class Solution38 {
+    public static void main(String[] args) {
+        //gets input from the user
+        //removes all odd numbers from the string
+        //prints out the new string
+    }
+
+    public int[] filterEvenNumbers(int[] fullArray) {
+        //returns an array with only even numbers
+    }
+
+    public int[] getUserArray() {
+        //prompts the user to enter a list of numbers separated by a single white space
+        //splits the user's String into an array
+        //returns the String
+    }
+}
---
+++ exercise38/src/test/java/baseline/Solution38Test.java
@@ -0,0 +1,27 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution38Test {
+    @Test
+    void filterEvenNumbersTest1() {
+        Solution38 solution = new Solution38();
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        assertEquals(0, solution.filterEvenNumbers(testArray)[0] % 2);
+        assertEquals(0, solution.filterEvenNumbers(testArray)[1] % 2);
+        assertEquals(0, solution.filterEvenNumbers(testArray)[2] % 2);
+        assertEquals(0, solution.filterEvenNumbers(testArray)[3] % 2);
+    }
+
+    @Test
+    void filterEvenNumbersTest2() {
+        Solution38 solution = new Solution38();
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] expected = {2, 4, 6, 8};
+        assertArrayEquals(expected, solution.filterEvenNumbers(testArray));
+    }
+
+
+}
